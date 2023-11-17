6558c4b3a7b3372275cc35866ced607e60256f39
Alibi <ayermeko@student.42prague.com>
2023-11-17 19:23:41 +0100

    fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,48 @@
 /*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:20:21 by ayermeko          #+#    #+#             */
-/*   Updated: 2023/11/17 17:36:51 by ayermeko         ###   ########.fr       */
+/*   Updated: 2023/11/17 17:53:52 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *ft_read_append(int fd, char backup)
+char *ft_read_to_left_str(int fd, char *backup)
 {
+    int bytes;
     char *buff;
-    int i;
-
+    
     buff = malloc(BUFFER_SIZE + 1);
     if (!buff)
         return (NULL);
-    i = 1;
-    while (!ft_strchr(backup, '\n') && i != 0)
+    bytes = 1;
+    while (!ft_find_newline(buff, '\n') && bytes != '\0')
     {
-        i = read(fd, buff, BUFFER_SIZE);
-        if (i == -1)
+        bytes = read(fd, buff, BUFFER_SIZE);
+        if (bytes == -1)
         {
             free(buff);
             return (NULL);
         }
-        buff[i] = '\0';
+        buff[bytes] = '\0';
         backup = ft_strjoin(backup, buff);
     }
     free(buff);
     return (backup);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char *line;
-    static char *backup;
+	char		*line;
+	static char	*backup;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    backup = ft_read_append(fd, backup);
-    if (!backup)
-        return (NULL);
-    line = ft_get_line(backup);
-    backup = ft_new_left_str(backup);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	backup = ft_read_to_left_str(fd, backup);
+	if (!backup)
+		return (NULL);
+	line = ft_get_line(backup);
+	backup = ft_new_backup(backup);
 	return (line);
 }
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:20:11 by ayermeko          #+#    #+#             */
-/*   Updated: 2023/11/17 17:03:07 by ayermeko         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:30:28 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,19 @@ size_t ft_strlen(char *s)
     return (i);
 }
 
-char	*ft_strjoin(char *left_str, char *buff)
+char *ft_strjoin(char *backup, char *buff)
 {
-	size_t	i;
-	size_t	j;
-	char	*str;
-
-	if (!left_str)
-	{
-		left_str = (char *)malloc(1 * sizeof(char));
-		left_str[0] = '\0';
-	}
-	if (!left_str || !buff)
-		return (NULL);
-	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
-	if (str == NULL)
-		return (NULL);
-	i = -1;
-	j = 0;
-	if (left_str)
-		while (left_str[++i] != '\0')
-			str[i] = left_str[i];
-	while (buff[j] != '\0')
-		str[i++] = buff[j++];
-	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
-	free(left_str);
-	return (str);
+	size_t i;
+	size_t j;
+	char *str;
+	
+    if (!backup)
+    {
+        backup = (char *)malloc(1 * sizeof(char));
+        backup[i] = "\0";
+    }
+	if (!backup || !buff)
+        return (NULL);
+    
+	
 }
